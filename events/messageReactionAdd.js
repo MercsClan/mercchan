7b0df568251c63ef238d3b9c87519c81db9d1896
Nathan Knight <nathan.t.knight@gmail.com>
2020-09-27 13:22:54 -0500

    Modified collectors: added AmongUs, removed Tarkov

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -4,18 +4,18 @@ module.exports = async (reaction, user, mercchan) => {
   const nUser = await reaction.message.guild.members.cache.get(user.id);
 
   // EmojiList
-  const emojiTarkov = 'EFT';
   const emojiRocket = '⚽';
   const emojiFlightSim = '✈️';
   const emojiFallGuys = 'fg';
   const emojiFactory = 'fb';
+  const emojoiAmongUs = 'among';
 
   //Role List
-  const roleTarkov = '745022672152559642';
   const roleRocket = '745023066995949689';
   const roleFlight = '745023174579585165';
   const roleFallGuys = '745081929153511554';
   const roleFactory = '745090173242703962';
+  const roleAmongUs = '759837704237088808';
 
   if (reaction.partial) {
     try {
@@ -33,12 +33,12 @@ module.exports = async (reaction, user, mercchan) => {
       nUser.roles.add(roleRocket);
     } else if (reaction.emoji.name === emojiFlightSim) {
       nUser.roles.add(roleFlight);
-    } else if (reaction.emoji.name === emojiTarkov) {
-      nUser.roles.add(roleTarkov);
     } else if (reaction.emoji.name === emojiFallGuys) {
       nUser.roles.add(roleFallGuys);
     } else if (reaction.emoji.name === emojiFactory) {
       nUser.roles.add(roleFactory);
+    } else if (reaction.emoji.name === emojoiAmongUs) {
+      nUser.roles.add(roleAmongUs);
     } else {
       console.log(`No Role Found: ${reaction.emoji.id}`);
     }
